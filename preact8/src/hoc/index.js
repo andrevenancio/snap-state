a6b41512df50e5748a2abe752b9cfd56baa70a34
andrevenancio <noreply@andrevenancio.com>
2019-08-11 18:49:46 +0100

    add support for preact8 until preactX is released widh hooks

---
+++ preact8/src/hoc/index.js
@@ -0,0 +1,30 @@
+import { h } from 'preact';
+import { onSnapState } from '../../../src/vanilla';
+
+const withSnapState = props => WrappedComponent => {
+    class SnapStateHOC extends Component {
+        state = {};
+
+        componentDidMount() {
+            this.tempHook = onSnapState(props, this.updateState);
+        }
+
+        componentWillUnmount() {
+            this.tempHook();
+        }
+
+        updateState = ({ key, value }) => {
+            this.setState({
+                [key]: value,
+            });
+        };
+
+        render() {
+            return <WrappedComponent {...this.state} />;
+        }
+    }
+
+    return SnapStateHOC;
+};
+
+export { withSnapState };
---
+++ preact8/src/index.js
@@ -0,0 +1,4 @@
+export * from '../../src/emitter';
+export * from '../../src/state';
+export * from './hoc';
+export * from '../../src/vanilla';
